2e9bf070317a5a175b745e9b3580d71ab066c0a2
Alex <maybe.baikal@gmail.com>
2024-11-18 17:06:35 +0900

    Add component TeleportObject

---
+++ Assets/_Asteroids/Scripts/TeleportObject.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace Scripts
+{
+    public class TeleportObject : MonoBehaviour
+    {
+        [field: SerializeField] public float AdditionDistance { get; private set; } = 1f;
+    }
+}
