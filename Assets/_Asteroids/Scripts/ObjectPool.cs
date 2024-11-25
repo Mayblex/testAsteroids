11298f0806269bee0d951dae6de41b05b5e16f56
Alex <maybe.baikal@gmail.com>
2024-11-25 14:32:22 +0900

    Create

---
+++ Assets/_Asteroids/Scripts/ObjectPool.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.Pool;
+
+namespace Scripts
+{
+    public class ObjectPool
+    {
+        private ObjectPool<GameObject> _pool;
+
+        private GameObject _prefab;
+
+        public ObjectPool(GameObject prefab, int prewarmObjectsCount)
+        {
+            _prefab = prefab;
+            _pool = new ObjectPool<GameObject>(OnCreateObject, OnGetObject, OnRelease, OnObjectDestroy, false,
+                prewarmObjectsCount);
+        }
+
+        public GameObject Get()
+        {
+            var obj = _pool.Get();
+            return obj;
+        }
+        
+        public void Release(GameObject obj)
+        {
+            _pool.Release(obj);
+        }
+        
+        private void OnObjectDestroy(GameObject obj)
+        {
+            GameObject.Destroy(obj);
+        }
+        
+        private void OnRelease(GameObject obj) => 
+            obj.gameObject.SetActive(false);
+
+        private void OnGetObject(GameObject obj) => 
+            obj.gameObject.SetActive(true);
+
+        private GameObject OnCreateObject() => 
+            Object.Instantiate(_prefab);
+    }
+}
