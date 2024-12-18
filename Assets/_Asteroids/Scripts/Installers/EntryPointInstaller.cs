b48d647d4ce9d548dbeb572af30506c4ae913dcb
Alex <maybe.baikal@gmail.com>
2024-12-18 21:21:29 +0900

    Add EntryPointInstaller

---
+++ Assets/_Asteroids/Scripts/Installers/EntryPointInstaller.cs
@@ -0,0 +1,16 @@
+﻿using _Asteroids.Scripts.Core;
+using Zenject;
+
+namespace _Asteroids.Scripts.Installers
+{
+    public class EntryPointInstaller : MonoInstaller
+    {
+        public override void InstallBindings()
+        {
+            Container.
+                BindInterfacesAndSelfTo<EntryPoint>().
+                AsCached().
+                NonLazy();
+        }
+    }
+}
