a4774a38758dc3ecf6c9740ea08fcbf136a4566e
Alex <maybe.baikal@gmail.com>
2024-12-25 22:31:33 +0900

    Add Bootstrap scene and BootstrapInstaller

---
+++ Assets/_Asteroids/Scripts/Core/Bootstrap.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading.Tasks;
+using _Asteroids.Scripts.Services;
+using UnityEngine;
+using Zenject;
+
+namespace _Asteroids.Scripts.Core
+{
+    public class Bootstrap : IInitializable
+    {
+        private readonly ZenjectSceneLoader _sceneLoader;
+        private readonly IAnalyticsService _analyticsService;
+        
+        public Bootstrap(ZenjectSceneLoader sceneLoader, IAnalyticsService analyticsService)
+        {
+            _sceneLoader = sceneLoader;
+            _analyticsService = analyticsService;
+        }
+        
+        public async void Initialize()
+        {
+            Debug.Log("Start");
+            
+            await _analyticsService.Initialize();
+            
+            _sceneLoader.LoadScene("MainScene");
+        }
+    }
+}
---
+++ Assets/_Asteroids/Scripts/Installers/BootstrapInstaller.cs
@@ -0,0 +1,16 @@
+﻿using _Asteroids.Scripts.Core;
+using Zenject;
+
+namespace _Asteroids.Scripts.Installers
+{
+    public class BootstrapInstaller : MonoInstaller
+    {
+        public override void InstallBindings()
+        {
+            Container.
+                BindInterfacesAndSelfTo<Bootstrap>().
+                AsSingle().
+                NonLazy();
+        }
+    }
+}
