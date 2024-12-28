1f5ad10e2673bbedf5dfb7f16a8d36a6e91545eb
Alex <maybe.baikal@gmail.com>
2024-12-28 23:58:30 +0900

    Add ShipHolder and subscribed to two events

--- Assets/_Asteroids/Scripts/Services/AnalyticsEventTracker.cs
+++ Assets/_Asteroids/Scripts/Services/AnalyticsEventTracker.cs
@@ -1,30 +1,45 @@
-﻿namespace _Asteroids.Scripts.Services
+﻿using _Asteroids.Scripts.Data;
+using _Asteroids.Scripts.Gameplay.Ship;
+
+namespace _Asteroids.Scripts.Services
 {
     public class AnalyticsEventTracker
     {
         private readonly IAnalyticsService _analyticsService;
-        private GameplayStatistics _gameplayStatistics;
-        
-        public AnalyticsEventTracker(IAnalyticsService analyticsService)
+        private readonly GameplayStatistics _gameplayStatistics;
+        private readonly ShipHolder _shipHolder;
+
+        public AnalyticsEventTracker(ShipHolder shipHolder, IAnalyticsService analyticsService, GameplayStatistics gameplayStatistics)
         {
             _analyticsService = analyticsService;
-            _gameplayStatistics = new GameplayStatistics();
+            _gameplayStatistics = gameplayStatistics;
+            _shipHolder = shipHolder;
         }
-        
+
+        public void Initialize()
+        {
+            _shipHolder.Ship.GetComponent<Ship>().Died += OnShipDied;
+            _shipHolder.Ship.GetComponent<Ship>().LaserShot += OnLaserShot;
+        }
+
         public void LogStartGameEvent() =>
             _analyticsService.LogEvent(AnalyticsEventName.GAME_START);
-        
-        public void LogEndGameEvent(GameplayStatistics gameplayStatistics)
+
+        private void LogEndGameEvent()
         {
             _analyticsService.LogEvent(AnalyticsEventName.GAME_OVER,
-                (AnalyticsEventName.BULLET_SHOT, gameplayStatistics.BulletShots),
-                (AnalyticsEventName.LASER_SHOTS, gameplayStatistics.LaserShots),
-                (AnalyticsEventName.DESTROYED_ASTEROIDS, gameplayStatistics.DestroyedAsteroids),
-                (AnalyticsEventName.DESTROYED_UFOS, gameplayStatistics.DestroyedUFOs)
+                (AnalyticsEventName.BULLET_SHOT, _gameplayStatistics.BulletShots),
+                (AnalyticsEventName.LASER_SHOTS, _gameplayStatistics.LaserShots),
+                (AnalyticsEventName.DESTROYED_ASTEROIDS, _gameplayStatistics.DestroyedAsteroids),
+                (AnalyticsEventName.DESTROYED_UFOS, _gameplayStatistics.DestroyedUFOs)
             );
         }
-        
-        public void LogLaserShot() =>
+
+        private void LogLaserShot() =>
             _analyticsService.LogEvent(AnalyticsEventName.LASER_SHOTS);
+
+        private void OnShipDied() => LogEndGameEvent();
+
+        private void OnLaserShot() => LogLaserShot();
     }
 }
