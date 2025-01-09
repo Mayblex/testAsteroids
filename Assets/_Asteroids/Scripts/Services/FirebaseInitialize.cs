30f7ff0425d675ea7fa2d3424c97eeb745490dcc
Alex <maybe.baikal@gmail.com>
2025-01-09 11:08:18 +0900

    Add FirebaseInitialize

---
+++ Assets/_Asteroids/Scripts/Services/FirebaseInitialize.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Tasks;
+using Firebase;
+using Firebase.Extensions;
+using UnityEngine;
+
+namespace _Asteroids.Scripts.Services
+{
+    public class FirebaseInitialize
+    {
+        public async Task InitializeFirebase()
+        {
+            await FirebaseApp.CheckAndFixDependenciesAsync().ContinueWithOnMainThread(task =>
+            {
+                if (task.IsCompleted)
+                {
+                    FirebaseApp app = FirebaseApp.DefaultInstance;
+                    Debug.Log("Firebase initialized successfully.");
+                }
+                else
+                {
+                    Debug.LogError("Firebase initialization failed: " + task.Exception);
+                }
+            });
+        }
+    }
+}
