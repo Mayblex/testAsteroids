67a6cd50a115820f676ccae381c2b978d33bf3a9
Alex <maybe.baikal@gmail.com>
2025-09-08 00:00:48 +0900

    Refactor

--- Assets/_Asteroids/Scripts/Services/SceneLoader.cs
+++ Assets/_Asteroids/Scripts/Services/SceneLoader.cs
@@ -5,6 +5,11 @@ namespace _Asteroids.Scripts.Services
     public class SceneLoader : ISceneLoader
     {
         private readonly ZenjectSceneLoader _sceneLoader;
+
+        public SceneLoader(ZenjectSceneLoader sceneLoader)
+        {
+            _sceneLoader = sceneLoader;
+        }
         
         public void LoadGame()
         {
