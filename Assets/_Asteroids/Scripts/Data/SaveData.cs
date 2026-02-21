eb34e6cbdeb8106a365d7246bfc036d6d666aef2
Aleksei Baikalov <maybe.baikal@gmail.com>
2026-02-21 22:29:51 +0900

    feat: add SaveData model

---
+++ Assets/_Asteroids/Scripts/Data/SaveData.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _Asteroids.Scripts.Data
+{
+    [Serializable]
+    public class SaveData
+    {
+        public int BestScore;
+        public bool NoAdsPurchased;
+        public long SaveAtUnix;
+    }
+}
