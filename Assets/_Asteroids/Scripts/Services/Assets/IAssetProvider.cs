16d1622f69ff1f0363a100b75796fb62a875da23
Aleksei Baikalov <maybe.baikal@gmail.com>
2026-03-16 01:34:11 +0900

    feat: add asset provider wrapper for addressables

---
+++ Assets/_Asteroids/Scripts/Services/Assets/IAssetProvider.cs
@@ -0,0 +1,13 @@
+﻿using Cysharp.Threading.Tasks;
+
+namespace _Asteroids.Scripts.Services.Assets
+{
+    public interface IAssetProvider
+    {
+        UniTask<T> LoadAssetAsync<T>(string address) where T : class;
+        UniTask Warmup(params string[] addresses);
+        T GetLoaded<T>(string address) where T : class;
+        void Release(string address);
+        void ReleaseAll();
+    }
+}
